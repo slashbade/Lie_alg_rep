fdac49a501b9e5f293ee2ccab8e3c40ca3ac574d
slashbade <slashbladevitali@gmail.com>
2024-02-27 18:43:21 +0800

    Update main file

---
+++ LieAlgRep.lean
@@ -0,0 +1,4 @@
+import LieAlgRep.Basic
+import LieAlgRep.GeneralLinear
+import LieAlgRep.Schur
+import LieAlgRep.Weyl
--- lakefile.lean
+++ lakefile.lean
@@ -11,3 +11,6 @@ lean_lib «LieAlgRep» where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4"
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
